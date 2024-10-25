a3a1521aa1c3c02c60295dd436d5cf659773b33b
Armiel Gonzales <161496207+armielgonzzz@users.noreply.github.com>
2024-10-25 15:59:33 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
-st.title("Customize Your Smoothie :cup_with_straw:")
+st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
     """
     Choose the fruits you want in your customer Smoothie!
@@ -39,4 +39,4 @@ if ingredient_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
