27d81ed484c320a3ad94ab91ba387026300d3f37
Armiel Gonzales <161496207+armielgonzzz@users.noreply.github.com>
2024-10-25 15:57:05 -0500

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,4 +1,5 @@
 # Import python packages
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -36,3 +37,6 @@ if ingredient_list:
         """
         session.sql(my_insert_stmt, params=[ingredient_string, name_on_order]).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
